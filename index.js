62d6966bccec9d1a33ca7ca5bb0a3cc5e7124215
SOWMYA <sowmyasriramula216@gmail.com>
2023-10-25 16:18:12 +0530

    Added codes

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const express =require('express')
 const socketio=require('socket.io')
 const http=require('http')
 
+
 const app=express()
 const server=http.createServer(app)
 const io=socketio(server)
 
 io.on('connection',(socket)=>{
     console.log('a user connected',socket.id);
-    socket.on('from_client',()=>{
-        console.log('event from client');
+    socket.on('msg_send',(data)=>{
+        console.log(data);
+        // io.emit('msg_rcvd',data)
+        socket.broadcast.emit('msg_rcvd',data)
     })
-    setInterval(()=>{
-        socket.emit('from_server')
-    },2000)
+    
 })
 app.use('/',express.static(__dirname + '/public'))
 
